68392015eea420cdc28ce4d2b3bf3529d6e979c8
Júnior Gonçalves <jrgoncalves85@gmail.com>
2020-05-18 19:39:27 -0300

    Commit inicial - layout do sistema

---
+++ admin/layout/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Sistema Escolar</a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Sair</a>
+    </li>
+  </ul>
+</nav>
