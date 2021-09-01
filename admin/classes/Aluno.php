051bff7c65ac88a87551c9d48b5b6bdd425ceae9
Rodrigo Tognin <rotog@outlook.com>
2021-09-01 07:20:04 -0300

    Melhorias de segurança ao executar os comandos SQL no banco
    Estou propondo o uso dos comandos "preparados" para melhoria na segurança em um suposto ataque de "SQL injection".

--- admin/classes/Aluno.php
+++ admin/classes/Aluno.php
@@ -25,20 +25,27 @@ class Aluno
 
     public function inserir()
     {
-        $sql = "INSERT INTO alunos (nome, email, idade, turma_id) VALUES (
-                    '" . $this->nome . "', 
-                    '" . $this->email . "',
-                    '" . $this->idade . "',
-                    '" . $this->turma_id . "')";
+        $sql = 'INSERT INTO alunos (nome, email, idade, turma_id) VALUES ' .
+               '(:nome, :email, :idade, :turma_id);
+        
         $conexao = new PDO('mysql:host=127.0.0.1;dbname=escolar','root','');
-        $conexao->exec($sql);
+        $stmt = $conexao->prepare($sql);
+        
+        $stmt->bindValue('nome', $this->nome);
+        $stmt->bindValue('email', $this->email);
+        $stmt->bindValue('idade', $this->idade, PDO::PARAM_INT);
+        $stmt->bindValue('turma_id', $this->turma_id, PDO::PARAM_INT);
+        
+        $stmt->execute();
     }
 
     public function excluir()
     {
-        $sql = "DELETE FROM alunos WHERE id=".$this->id;
+        $sql = 'DELETE FROM alunos WHERE id = :id';
         $conexao = new PDO('mysql:host=127.0.0.1;dbname=escolar','root','');
-        $conexao->exec($sql);
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindValue('id', $this->id, PDO::PARAM_INT);
+        $stmt->execute();
     }
 
 }
